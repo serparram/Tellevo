266f35f6ce725835e42388a36ee62601e1c418c1
se.parram <se.parram@duocuc.cl>
2021-12-15 13:51:47 -0300

    git

--- src/app/services/viajes.service.ts
+++ src/app/services/viajes.service.ts
@@ -7,7 +7,7 @@ import { Viajes } from './../interfaces/viajes';
   providedIn: 'root'
 })
 export class ViajesService {
-  private api = 'https://raw.githubusercontent.com/serparram/Tellevo/serparram/db.json';
+  private api = 'https://raw.githubusercontent.com/serparram/Tellevo/main/db.json';
 
   constructor( private http: HttpClient,) {
    }
