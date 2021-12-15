35cf0e9d21c9674943c52c41058cc64e4a5828fa
se.parram <se.parram@duocuc.cl>
2021-12-15 13:45:51 -0300

    modificacion git

--- src/app/services/viajes.service.ts
+++ src/app/services/viajes.service.ts
@@ -7,7 +7,7 @@ import { Viajes } from './../interfaces/viajes';
   providedIn: 'root'
 })
 export class ViajesService {
-  private api = 'http://localhost:3000';
+  private api = 'https://raw.githubusercontent.com/serparram/Tellevo/main/db.json';
 
   constructor( private http: HttpClient,) {
    }
